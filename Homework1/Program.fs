e8fac73656a6793dabfe0fb2e37072fce8ca5e3d
Ksenia Nigmatulina <kv.nigma@gmail.com>
2024-09-05 23:55:42 +0300

    Добавила решение первого домашнего задания

---
+++ Homework1/Program.fs
@@ -0,0 +1,32 @@
+﻿
+let findFirstOccurance x ls = 
+    let rec findFirstOccuranceInternal index x ls = 
+        if ls = [] then -1
+        elif ls.Head = x then index
+        else findFirstOccuranceInternal (index + 1) x ls.Tail
+    findFirstOccuranceInternal 0 x ls
+
+
+let createLsPowOfTwo n m =
+    let rec raiseToPower number power result =
+        if (power = 0) then result
+        elif (power % 2 <> 0) then raiseToPower number (power - 1) (result * number)
+        else raiseToPower (number * number) (power / 2) result
+    List.scan(fun acc x-> acc * 2) (raiseToPower 2 (n) 1) [ for i in n..(m + n - 1) -> i ]
+
+
+let invertList () = Seq.fold (fun acc x -> x :: acc) []
+
+
+let getNFibonacciNumber n = 
+    let rec nFibonacciNumberInternal prevPrev prev n = 
+        if n = 0 then prevPrev
+        else nFibonacciNumberInternal prev (prevPrev+ prev) (n - 1)
+    nFibonacciNumberInternal 0 1 n
+
+
+let rec getNFactorial n =
+    let rec nFactorialInternal acc n = 
+        if n = 0 then acc
+        else nFactorialInternal (acc * n) (n - 1)
+    nFactorialInternal 1 n
