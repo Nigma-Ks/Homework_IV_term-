51b694cffb9f0fd1b976803b71a6b680e496259d
Ksenia Nigmatulina <kv.nigma@gmail.com>
2024-09-09 17:36:01 +0300

    +решение 1 задачи 2 домашнего задания

---
+++ PrimeNumbersSeq.Tests/PrimeNumberSeqTests.fs
@@ -0,0 +1,9 @@
+﻿module PrimeNumberSeq.Tests
+
+open NUnit.Framework
+open FsUnit
+open PrimeNumberSeqGenerator
+
+[<Test>]
+let ``First 6 generated prime numbers should be correct`` () =
+     createPrimeNumbersInfSeq () |> Seq.take 6 |> Seq.toList |> should equal [2; 3; 5; 7; 11; 13]
---
+++ PrimeNumbersSeq/PrimeNumberSeq.fs
@@ -0,0 +1,18 @@
+﻿module PrimeNumberSeqGenerator =
+    let createPrimeNumbersInfSeq () =
+        let isPrime number =
+                let sqrtNumber = int (sqrt(float number))
+                let rec isPrimeInternal currDivider =
+                    if currDivider > sqrtNumber then true
+                    elif number % currDivider = 0 then false
+                    else isPrimeInternal (currDivider + 1)
+                isPrimeInternal 2
+        let rec generatePrimes currentNumber =
+            if isPrime currentNumber then
+                seq {
+                    yield currentNumber
+                    yield! generatePrimes (currentNumber + 1)
+                }
+            else
+                generatePrimes (currentNumber + 1)
+        generatePrimes 2
